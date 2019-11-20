5a0a91eeb234407c25778a2cd29eb58ff56dd810
djsjollema <j.sjollema@ma-web.nl>
2019-11-20 12:42:35 +0100

    first commit

---
+++ 01_huisje/script.js
@@ -0,0 +1,13 @@
+// maak een variabele voor de canvas
+const canvas = document.getElementById('canvas');
+
+// leg een 2d-context over de canvas
+const context = canvas.getContext('2d');
+
+//maak de canvas schermvullend
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+// hier kun je op de canvas tekenen

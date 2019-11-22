6db464eb0ef2e743e38450a4aa91a4291e36eba7
Thomas Berrens <thomas.berrens@me.com>
2019-11-22 13:43:45 +0100

    osso

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -11,3 +11,42 @@ canvas.width = width;
 canvas.height = height;
 
 // hier kun je op de canvas tekenen
+
+context.beginPath()
+context.fillStyle = "red";
+context.moveTo(300, 100);
+context.lineTo(700, 200);
+context.lineTo(600, 400);
+context.lineTo(200, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "gray";
+context.moveTo(200, 300)
+context.lineTo(200, 500)
+context.lineTo(600, 600)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "gray";
+context.moveTo(600, 400)
+context.lineTo(700, 200)
+context.lineTo(800, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "gray";
+context.moveTo(600, 600)
+context.lineTo(800, 500)
+context.lineTo(800, 300)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
